82d54aa80c768552f324480fbd8826773e94294c
Christoph Pirkl <christoph.pirkl@exasol.com>
2022-03-02 18:31:30 +0100

    Fix luacheck warning

--- test/smoketest.lua
+++ test/smoketest.lua
@@ -6,7 +6,8 @@ function test_version() luaunit.assertEquals(driver.VERSION, "0.1.0") end
 function test_connection_fails()
     local env = driver.exasol({log_level = "TRACE"})
     luaunit.assertErrorMsgContentEquals(
-        "E-EDL-1: Error connecting to 'wss://localhost:1234': 'Connection to localhost:1234 failed: connection refused'",
+        "E-EDL-1: Error connecting to 'wss://localhost:1234': " ..
+            "'Connection to localhost:1234 failed: connection refused'",
         function() env:connect("localhost:1234", "user", "password") end)
     env:close()
 end
