d01e113df3588a4e2415cc79b3832c51acdef98c
Christoph Pirkl <christoph.pirkl@exasol.com>
2022-05-06 07:55:01 +0200

    #60 Verify consistent version numbers

---
+++ tools/verify-version-numbers.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+base_dir="$( cd "$(dirname "$0")/.." >/dev/null 2>&1 ; pwd -P )"
+readonly base_dir
+
+# shellcheck disable=SC2046 # word splitting is OK here
+lua "$base_dir/tools/verify-version-numbers.lua" $(find "$base_dir" -name "*.rockspec")
