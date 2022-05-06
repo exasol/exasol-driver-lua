adfd5690bed301f3826f4ae1f865d34d12326a42
chπ <christoph.pirkl@exasol.com>
2022-05-06 11:54:10 +0200

    #60 Verify consistent version numbers (#61)
    * #60 Verify consistent version numbers

--- src/luasql/exasol/constants.lua
+++ src/luasql/exasol/constants.lua
@@ -5,7 +5,7 @@ local constants = {}
 local util = require("luasql.exasol.util")
 
 --- The version of this module
-constants.VERSION = "0.1.0"
+constants.VERSION = "0.1.0-1"
 
 --- The value returned by queries to indicate an SQL `NULL` value.
 constants.NULL = {}
