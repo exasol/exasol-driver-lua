3bbfb52fce53e5055f76ef5f2df5433bdc13a623
Christoph Pirkl <christoph.pirkl@exasol.com>
2022-05-06 07:05:32 +0200

    Unify paths

--- tools/format-lua.sh
+++ tools/format-lua.sh
@@ -7,7 +7,7 @@ set -o pipefail
 base_dir="$( cd "$(dirname "$0")/.." >/dev/null 2>&1 ; pwd -P )"
 readonly base_dir
 
-readonly src_module_path="$base_dir/src/"
+readonly src_module_path="$base_dir/src"
 readonly test_module_path="$base_dir/spec"
 
 # Don't format third party code
