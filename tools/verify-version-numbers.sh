28505544e9595762c5b501f670d2d117611b96df
Christoph Pirkl <christoph.pirkl@exasol.com>
2022-05-06 08:08:37 +0200

    Find only files in project dir
    lua: ...r-lua/exasol-driver-lua/tools/verify-version-numbers.lua:14: Expected 1 argument but got 8: /home/runner/work/exasol-driver-lua/exasol-driver-lua/exasol-driver-lua-0.1.0-1.rockspec /home/runner/work/exasol-driver-lua/exasol-driver-lua/.luarocks/lib/luarocks/rocks-5.4/say/1.3-1/say-1.3-1.rockspec /home/runner/work/exasol-driver-lua/exasol-driver-lua/.luarocks/lib/luarocks/rocks-5.4/lua-term/0.7-1/lua-term-0.7-1.rockspec /home/runner/work/exasol-driver-lua/exasol-driver-lua/.luarocks/lib/luarocks/rocks-5.4/lua_cliargs/3.0-2/lua_cliargs-3.0-2.rockspec /home/runner/work/exasol-driver-lua/exasol-driver-lua/.luarocks/lib/luarocks/rocks-5.4/busted/2.0.0-1/busted-2.0.0-1.rockspec /home/runner/work/exasol-driver-lua/exasol-driver-lua/.luarocks/lib/luarocks/rocks-5.4/mediator_lua/1.1.2-0/mediator_lua-1.1.2-0.rockspec /home/runner/work/exasol-driver-lua/exasol-driver-lua/.luarocks/lib/luarocks/rocks-5.4/luasystem/0.2.1-0/luasystem-0.2.1-0.rockspec /home/runner/work/exasol-driver-lua/exasol-driver-lua/.luarocks/lib/luarocks/rocks-5.4/luassert/1.8.0-0/luassert-1.8.0-0.rockspec

--- tools/verify-version-numbers.sh
+++ tools/verify-version-numbers.sh
@@ -8,4 +8,4 @@ base_dir="$( cd "$(dirname "$0")/.." >/dev/null 2>&1 ; pwd -P )"
 readonly base_dir
 
 # shellcheck disable=SC2046 # word splitting is OK here
-lua "$base_dir/tools/verify-version-numbers.lua" $(find "$base_dir" -name "*.rockspec")
+lua "$base_dir/tools/verify-version-numbers.lua" $(find "$base_dir" -maxdepth 1 -name "*.rockspec")
