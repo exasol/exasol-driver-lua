d4324154d47795a193b5036c1db642ec30e7abf5
Christoph Pirkl <4711730+kaklakariada@users.noreply.github.com>
2024-04-29 09:47:16 +0200

    Fix publishing github pages (#93)

--- tools/build-docs.sh
+++ tools/build-docs.sh
@@ -7,7 +7,7 @@ set -o pipefail
 base_dir="$( cd "$(dirname "$0")/.." >/dev/null 2>&1 ; pwd -P )"
 readonly base_dir
 
-readonly target_dir="$base_dir/target/ldoc"
+readonly target_dir="$base_dir/target/ldoc/api/"
 mkdir --parents "$target_dir"
-ldoc --config "$base_dir/config.ld" --verbose --fatalwarnings .
+ldoc --config "$base_dir/config.ld" --dir "$target_dir" --verbose --fatalwarnings .
 echo "ldoc result: $?"
